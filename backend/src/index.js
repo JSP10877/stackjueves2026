38bfef0954179afc3cf44dd3c0872a88574f7fc6
JSP10877 <juansebastianpinto10877@gmail.com>
2026-08-20 14:43:16 -0500

    repositorio jueves

---
+++ backend/src/index.js
@@ -0,0 +1,10 @@
+import express from "express"
+import morgan from "morgan"
+import {connectDB} from "./db.js"
+const app = express()
+
+app.use(morgan('dev')); 
+connectDB()
+
+app.listen(4000)
+console.log("Server is running on port 4000") 
